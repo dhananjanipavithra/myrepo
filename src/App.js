6dfaf21a44422a1c304ab1ef9ad4247be5505359
dhananjanipavithra <dhananjanipavithra@gmail.com>
2023-03-14 10:28:23 +0530

    changed name and email

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import AnimatedRoutes from "./components/AnimatedRoutes";
 
 function App() {
   const personalDetails = {
-    name: "Michael Yeates",
-    location: "Loughborough, UK",
-    tagline: "I'm a Developer",
-    email: "michael-yeates@outlook.com",
+    name: "Pavithra Dhananjani",
+    location: "Sri Lanka",
+    tagline: "Undergraduate",
+    email: "dhananjanipavithra@gmail.com",
     availability: "Open for work",
     brand:
       "My unique blend of technical expertise, creative thinking, and background in psychology allows me to approach each project with a deep understanding of the end user's perspective, resulting in highly effective user-centred digital products.",
